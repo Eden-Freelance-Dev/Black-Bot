4b3c973a409ffe3c3c360cc5cd8318a37e8734aa
Eden <edencheung5112006@gmail.com>
2020-10-18 00:07:25 +0800

    before delivery 2

--- autorole.js
+++ autorole.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 module.exports = {
     async onReactionAdd (reaction, user) {
+        console.log(reaction.emoji.name);
         if (reaction.partial) {
             try {
                 await reaction.fetch();
@@ -14,14 +15,16 @@ module.exports = {
 
         const autoRole = JSON.parse(fs.readFileSync("auto-role.json"));
         if(autoRole[reaction.message.id]){
-            if(autoRole[reaction.message.id]["reaction"] == reaction.emoji.name){
-                const role = await reaction.message.guild.roles.fetch(autoRole[reaction.message.id]["role"]);
-                const member = await reaction.message.guild.members.fetch(user.id);
-                await member.roles.add(role)
-                .catch((error) => {
-                    console.log(error);
-                });
-            }
+            autoRole[reaction.message.id].forEach(async element => {
+                if(element["reaction"] == reaction.emoji.name){
+                    const role = await reaction.message.guild.roles.fetch(element["role"]);
+                    const member = await reaction.message.guild.members.fetch(user.id);
+                    await member.roles.add(role)
+                    .catch((error) => {
+                        console.log(error);
+                    });
+                }
+            });
         }
     },
     async onReactionRemove(reaction, user){
@@ -36,14 +39,16 @@ module.exports = {
 
         const autoRole = JSON.parse(fs.readFileSync("auto-role.json"));
         if(autoRole[reaction.message.id]){
-            if(autoRole[reaction.message.id]["reaction"] == reaction.emoji.name){
-                const role = await reaction.message.guild.roles.fetch(autoRole[reaction.message.id]["role"]);
-                const member = await reaction.message.guild.members.fetch(user.id);
-                await member.roles.remove(role)
-                .catch((error) => {
-                    console.log(error);
-                });
-            }
+            autoRole[reaction.message.id].forEach(async element => {
+                if(element["reaction"] == reaction.emoji.name){
+                    const role = await reaction.message.guild.roles.fetch(element["role"]);
+                    const member = await reaction.message.guild.members.fetch(user.id);
+                    await member.roles.remove(role)
+                    .catch((error) => {
+                        console.log(error);
+                    });
+                }
+            });
         }
     }
 }
